e39a9a56f777a960b260aa271acd9e7c60bb16a8
Sharon Cartagena <sharoncartagenabarahona@gmail.com>
2024-02-15 22:38:57 -0600

    archivos

---
+++ components/Tabs.jsx
@@ -0,0 +1,36 @@
+//Iconografia
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MasVistos } from './pantallas/MasVistos';
+import { Nuevos } from './pantallas/Nuevos';
+import { Settings } from './pantallas/Settings';
+
+const Tab = createBottomTabNavigator();
+
+export const Tabs = () => {
+    return <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) =>{
+            let nombreIcono
+
+            switch (route.name) {
+                case 'MasVistos':
+                    nombreIcono = focused ? "favorite" : "favorite-outline"
+                    break;
+
+                case 'Nuevos':
+                    nombreIcono = focused ? "star" : "star-border"
+                    break;
+            
+                default:
+                    nombreIcono = focused ? "settings" : "settings-applications"
+                    break;
+            }
+            return <Icon name={nombreIcono} size={size} color={color} />
+        }
+    })}>
+                <Tab.Screen name="MasVistos" component={MasVistos} />
+                <Tab.Screen name="Nuevos" component={Nuevos} />
+                <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+}
